bd5222c17b255847281e656e7830ff1f843e8fb4
sergeiissaev <sergei740@live.ca>
2022-11-13 18:13:12 -0500

    fixed requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup
+
+setup(
+    name="finances",
+    packages=find_packages(),
+    version="1.1.0",
+    description="Net worth calculation package",
+    author="Sergei Issaev",
+)
