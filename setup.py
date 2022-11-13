9af75e53e5f66afbea6702bb626ec4663ed0feba
sergeiissaev <sergei740@live.ca>
2022-11-13 18:23:28 -0500

    added setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from setuptools import find_packages, setup
 
 setup(
     name="finances",
-    packages=find_packages(),
-    version="1.1.0",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    version="0.1.0",
     description="Net worth calculation package",
     author="Sergei Issaev",
 )
