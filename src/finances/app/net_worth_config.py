733f58b69cce153c176bcd476c43a89ec3dd549d
sergeiissaev <sergei740@live.ca>
2022-11-14 16:08:00 -0500

    fixed requirements

---
+++ src/finances/app/net_worth_config.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from pathlib import Path
+
+NET_WORTH_CONFIG = dict(
+    data_files_path=Path("data", "money"),
+    historical_net_worths=Path("data", "net_worth_history", "net_worth_history.csv"),
+)
